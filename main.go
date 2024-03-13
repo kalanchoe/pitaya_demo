f7290c0c0260e1095ee2718c3b2be762f24d4d34
fanyi <zhangfanyi@papegames.net>
2024-03-13 23:18:09 +0800

    Revert "更换为websocket"
    This reverts commit dab4d3125f849dea267b3e2ce5b8cfe466d49243.

--- main.go
+++ main.go
@@ -141,7 +141,7 @@ func createAcceptor(port int, reporters []metrics.Reporter) acceptor.Acceptor {
 
 	rateLimitConfig := config.NewPitayaConfig(pConfig).Conn.RateLimiting
 
-	tcp := acceptor.NewWSAcceptor(fmt.Sprintf(":%d", port))
+	tcp := acceptor.NewTCPAcceptor(fmt.Sprintf(":%d", port))
 	return acceptorwrapper.WithWrappers(
 		tcp,
 		acceptorwrapper.NewRateLimitingWrapper(reporters, rateLimitConfig))
